32b0401564645748d4f3636820e32390a046754b
Artem Khodyush <greenkaa@gmail.com>
2022-06-04 16:46:13 -0700

    use babel-plugin-dom-expressions from solid-devtools multirepo
    had to disable HMR
    
    also had to add vite special handling for cjs source dependencies (see https://vitejs.dev/guide/dep-pre-bundling.html#monorepos-and-linked-dependencies)
    
    also had to force-add assert as dependency of @babel/helper-module-imports in the toplevel .yarnrc.yml

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,20 @@ import solidPlugin from 'vite-plugin-solid';
 import WindiCSS from 'vite-plugin-windicss';
 
 export default defineConfig((env) => ({
-  plugins: [solidPlugin(), WindiCSS()],
+  plugins: [solidPlugin({hot: false}), WindiCSS()],
   define: {
     'process.env.BABEL_TYPES_8_BREAKING': 'true',
     'process.env.NODE_DEBUG': 'false',
     ...(env.command == 'build' ? {} : { global: 'globalThis' }),
   },
+  optimizeDeps: {
+    include: ['babel-preset-solid', 'babel-plugin-jsx-dom-expressions', '@babel/helper-module-imports']
+  },
   build: {
     target: 'esnext',
+    commonjsOptions: {
+      include: [/babel-preset-solid/, /babel-plugin-jsx-dom-expressions/, /@babel\/helper-module-imports/, /node_modules/]
+    },
     rollupOptions: {
       output: {
         manualChunks: {},
